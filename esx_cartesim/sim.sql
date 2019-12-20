be2d8ec380cf0b60aeb068e2a45dd9f365c3ac94
dada168011 <57065217+dada168011@users.noreply.github.com>
2019-12-20 14:55:07 +0100

    Add files via upload

---
+++ esx_cartesim/sim.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `vehicle_sold` (
+  `client` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `plate` varchar(50) NOT NULL,
+  `soldby` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
